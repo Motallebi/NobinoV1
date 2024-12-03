07d1083870182fea37023fbeb49d4352903a3f70
Seyed Massoud <smm.motallebi@gmail.com>
2024-12-03 06:15:07 -0800

    mm

--- app/src/main/java/com/smcdeveloper/nobinoapp/ui/screens/home/Movies.kt
+++ app/src/main/java/com/smcdeveloper/nobinoapp/ui/screens/home/Movies.kt
@@ -35,6 +35,7 @@ fun getAllMovies(viewModel: HomeViewModel = hiltViewModel())
                     Log.d(LOG_TAG,"---------"+result.message.toString())
                     Log.d(LOG_TAG,"---88------"+result.message.toString())
                     Log.d(LOG_TAG,"---8877------"+result.message.toString())
+                    Log.d(LOG_TAG,"---887888------"+result.message.toString())
 
                     Log.d(LOG_TAG,"success result"+result.data.toString())
 
